1b6ee85549dda0e755b09628ca0c7668fa021902
gtxx <xiongxiong1986@gmail.com>
2020-07-31 07:15:27 +0800

    init

---
+++ bgsimulator/main.py
@@ -0,0 +1,38 @@
+from enum import Enum
+class Card:bn
+    def __init__(self, minion: 'Minion'):
+        self.minion = minion
+
+class MinionType(Enum):
+    BEAST = 1
+    DEMON = 2
+    DRAGON = 3
+    MECH = 4
+    MURLOC = 5
+    PIRATE = 6
+    NEUTRAL = 7
+
+
+
+class Minion:
+    def __init__(self, trie: int, attack, blood):
+        self.trie = int
+        self.is_gold = False
+        self.attack = attack
+        self.blood = blood
+
+    @classmethod
+    def fuse_to_gold(cls, minion1: Minion, minion2: Minion, minion3: Minion):
+
+
+class Hero:
+    pass
+
+class Alleycat(Minion):
+    def __init__(self):
+        pass
+
+
+
+class Deathrattle:
+    pass
---
+++ tests/test_basic_battle.py
@@ -0,0 +1,9 @@
+from bgsimulator.main import Minion
+
+class FooMinion(Minion):
+    def __init__(self):
+        pass
+
+
+def test_2_basic_minion_battle():
+    pass
